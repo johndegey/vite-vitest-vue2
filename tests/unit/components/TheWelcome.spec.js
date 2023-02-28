5e3db9417e18561f2952d1ce7abf275eb8ffc6b0
thisis myname <Thisis.my@email.com>
2023-02-28 15:35:53 +0100

    Add unplugin-vue-components
    Remove all imports in script setup
    TheWelcome.spec.js
      - one test stubs all child components : test passes with no warnings
      - one test not : test passes but give vue warnings for all child components

--- tests/unit/components/TheWelcome.spec.js
+++ tests/unit/components/TheWelcome.spec.js
@@ -2,9 +2,26 @@ import {render, screen} from '@testing-library/vue';
 import TheWelcome from '@/components/TheWelcome.vue';
 
 describe('TheWelcome Test Suite', () => {
-  it('should render the component', () => {
+  it('should render the component with subbing and no warnings', () => {
+    render(TheWelcome, {
+      stubs: {
+        WelcomeItem: true,
+        DocumentationIcon: true,
+        ToolingIcon: true,
+        EcosystemIcon: true,
+        CommunityIcon: true,
+        SupportIcon: true,
+      },
+    });
+    // screen.debug();
+    expect(screen.getByTestId('welcome-documentation')).toBeInTheDocument();
+    expect(screen.getByTestId('welcome-tooling')).toBeInTheDocument();
+    expect(screen.getByTestId('welcome-ecosystem')).toBeInTheDocument();
+  });
+
+  it('should render the component with unknown components warnings', () => {
     render(TheWelcome);
-    screen.debug();
+    // screen.debug();
     expect(screen.getByTestId('welcome-documentation')).toBeInTheDocument();
     expect(screen.getByTestId('welcome-tooling')).toBeInTheDocument();
     expect(screen.getByTestId('welcome-ecosystem')).toBeInTheDocument();
